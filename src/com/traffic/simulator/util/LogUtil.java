1c247dfbac5e531efb9ef0fbea77c81b48aa77c6
Dao Truong Son <daotruongson2006@gmail.com>
2026-03-17 16:11:00 +0700

    submit

--- src/com/traffic/simulator/util/LogUtil.java
+++ src/com/traffic/simulator/util/LogUtil.java
@@ -1,12 +1,14 @@
 package com.traffic.simulator.util;
 
+import com.traffic.simulator.entity.VehicleType;
+
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class LogUtil {
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
 
-    public static synchronized void log(String message) {
-        System.out.println("[Time: " + LocalTime.now().format(formatter) + "] " + message);
+    public static synchronized void log(String message, VehicleType type) {
+        System.out.println("[Time: " + LocalTime.now().format(formatter) + "][ " + type + " ] " + message);
     }
 }
